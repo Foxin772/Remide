6ec68a474604b934cd23ef84eddbd486813b750e
Foxin772 <net-kda772@yandex.ru>
2016-03-24 17:11:36 +0700

    Base Project

---
+++ Remid/app/src/main/java/com/qoobico/remid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qoobico.remid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by denci on 24.03.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
